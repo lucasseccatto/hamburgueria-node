43451a3afc09b31a87d87bda344edd38ddb992c4
lucasseccatto <lucasseccatto1@gmail.com>
2023-07-20 13:53:14 -0300

    adicionado cors e vercel

--- index.js
+++ index.js
@@ -1,86 +1,98 @@
-const express = require('express')
-const uuid = require('uuid')
+const express = require('express');
+const uuid = require('uuid');
+const cors = require('cors');
 
-const port = 3001
-const app = express()
-app.use(express.json())
+const port = 3001;
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-const orders = []
+const orders = [];
 
 const checkOrderId = (req, res, next) => {
-    const { id } = req.params
+  const { id } = req.params;
 
-    const index = orders.findIndex(purchaseOrder => purchaseOrder.id === id)
+  const index = orders.findIndex((purchaseOrder) => purchaseOrder.id === id);
 
-    if (index < 0) {
-        return res.status(404).json({ error: "User not found" })
-    }
+  if (index < 0) {
+    return res.status(404).json({ error: 'User not found' });
+  }
 
-    req.orderIndex = index
-    req.orderId = id
+  req.orderIndex = index;
+  req.orderId = id;
 
-    next()
-}
+  next();
+};
 
 const methodUrl = (req, res, next) => {
-    console.log('Request Type:', req.method)
-    console.log('Request URL:', req.originalUrl)
+  console.log('Request Type:', req.method);
+  console.log('Request URL:', req.originalUrl);
 
-    next()
-}
+  next();
+};
 
 app.post('/orders', methodUrl, (req, res) => {
-    const { order, clientName, price } = req.body
+  const { order, clientName, price } = req.body;
 
-    const purchaseOrder = { id: uuid.v4(), order, clientName, price, status: "Em preparação" }
+  const purchaseOrder = {
+    id: uuid.v4(),
+    order,
+    clientName,
+    price,
+    status: 'Em preparação',
+  };
 
-    orders.push(purchaseOrder)
-    
-    return res.status(201).json(purchaseOrder)
-})
+  orders.push(purchaseOrder);
+
+  return res.status(201).json(purchaseOrder);
+});
 
 app.get('/orders', methodUrl, (req, res) => {
-    return res.json(orders)
-})
+  return res.json(orders);
+});
 
 app.put('/orders/:id', checkOrderId, methodUrl, (req, res) => {
-    const { order, clientName, price } = req.body
-    const index = req.orderIndex
-    const id = req.orderId
+  const { order, clientName, price } = req.body;
+  const index = req.orderIndex;
+  const id = req.orderId;
 
-    const updateOrder = { id, order, clientName, price, status: "Em preparação " }
+  const updateOrder = {
+    id,
+    order,
+    clientName,
+    price,
+    status: 'Em preparação ',
+  };
 
-    orders[index] = updateOrder
+  orders[index] = updateOrder;
 
-    return res.json(updateOrder)
-})
+  return res.json(updateOrder);
+});
 
 app.delete('/orders/:id', checkOrderId, methodUrl, (req, res) => {
-    const index = req.orderIndex
+  const index = req.orderIndex;
 
-    orders.splice(index, 1)
+  orders.splice(index, 1);
 
-    return res.status(204).json()
-})
+  return res.status(204).json();
+});
 
 app.get('/orders/:id', checkOrderId, methodUrl, (req, res) => {
-    const index = req.orderIndex
-    const purchaseOrder = orders[index]
+  const index = req.orderIndex;
+  const purchaseOrder = orders[index];
 
-    return res.json(purchaseOrder)
-})
+  return res.json(purchaseOrder);
+});
 
 app.patch('/orders/:id', checkOrderId, methodUrl, (req, res) => {
-    const index = req.orderIndex
-    const purchaseOrder = orders[index]
+  const index = req.orderIndex;
+  const purchaseOrder = orders[index];
 
-    purchaseOrder.status = "Pronto"
+  purchaseOrder.status = 'Pronto';
 
-    return res.json(purchaseOrder)
-})
-
-
+  return res.json(purchaseOrder);
+});
 
 app.listen(port, () => {
-    console.log(`🚀 Server started on port ${port}`)
-})
+  console.log(`🚀 Server started on port ${port}`);
+});
